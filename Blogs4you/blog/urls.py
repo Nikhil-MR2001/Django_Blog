6860108df95956ed2738a8627e5e85805548e951
Nikhil <Nikhil@gmail.com>
2023-12-27 19:26:23 +0530

    added frontend for our project (index.html)

--- Blogs4you/blog/urls.py
+++ Blogs4you/blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),
 ]
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Blogs4you/blog/views.py
+++ Blogs4you/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-
+from .models import Category, Post
 
 # Create your views here.
 
-def home(request):
-    pass
+def index(request):
+    c = Category.objects.all()[:6]
+    posts = Post.objects.all()[:4]
+    return render(request, 'index.html',
+                  {
+                      'categories':c,
+                      'posts':posts
+                  })
